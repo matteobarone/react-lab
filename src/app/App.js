4874dd13f7c9dd9af46c665ea6adab58a593ff60
Matteo B <matteobarone90@alice.it>
2018-01-29 13:01:00 +0100

    counter in redux

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Todo from './todo/todo';
 import Chrono from './chrono/chrono';
 import './App.css';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Router basename="/react">
-          <div>
-            {Header()}
-            {Content()}
-          </div>
-        </Router>
-      </div>
-    );
-  }
-}
+export const App = () => {
+  ReactDOM.render(
+    <div className="App">
+      <Router basename="/react">
+        <div>
+          {Header()}
+          {Content()}
+        </div>
+      </Router>
+    </div>,
+    document.getElementById('root')
+  );
+};
 
 const Header = () => (
   <header>
@@ -32,6 +33,13 @@ const Content = () => (
     <Route exact={true} path="/" component={Home}/>
     <Route path="/todo" component={Todo}/>
     <Route path="/chronometer" component={Chrono}/>
+    <Route path="/redux-counter" render={() => (
+      <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+      />
+    )}/>
   </main>
 );
 
@@ -41,6 +49,27 @@ const Home = () => (
     <ol>
       <li><Link to="/todo"> Todos </Link></li>
       <li><Link to="/chronometer"> Chronometer </Link></li>
+      <li><Link to="/redux-counter"> Redux counter </Link></li>
     </ol>
   </div>
 );
+
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}> + </button>
+    <button onClick={onDecrement}> - </button>
+  </div>
+);
+
+const counterReducer = (state = 0, action) => {
+  console.log(store.getState());
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state - 1;
+    default: return state;
+  }
+};
+
+const store = createStore(counterReducer);
+store.subscribe(App);
